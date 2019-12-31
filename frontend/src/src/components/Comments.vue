0d4922627e3d181b0af1c06a697d50fccb07fa75
yayoec <yayoec@gmail.com>
2019-12-31 15:33:58 +0800

    add frontend

---
+++ frontend/src/src/components/Comments.vue
@@ -0,0 +1,24 @@
+
+<template>
+  <div id="comments" class="comment-list">
+	<wb:comments url="auto" fontsize="14" width="auto" color="aad3ea,fafdff,4c4c4c,5093d5,cccccc,b1dcff" >
+	</wb:comments>
+</div>
+</template>
+<script>
+export default {
+  name: 'Comments',
+  props: {},
+  data() {
+    return {
+
+    }
+  },
+  mounted() {
+
+  },
+  methods() {
+    
+  }
+}
+</script>
